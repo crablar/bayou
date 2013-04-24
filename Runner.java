5a5b52f3ba772448daa913abf094d9292c559541
Jeff Meyerson <meff.jeyerson@gmail.com>
2013-04-24 17:46:10 -0500

    added husk

---
+++ Runner.java
@@ -0,0 +1,22 @@
+/**
+* Bayou
+* @author Jeff
+*/
+
+public class Runner
+{
+	private static String scriptName;
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Chao.  Welcome to Bayou.");
+		if(args.length == 1)
+			scriptName = args[0];
+		run();
+	}
+	
+	private void run(){
+		
+	}
+	
+}
