4a84cb49d5a7c37a3b9e08da127544c7cd01cae2
Jeff Meyerson <meff.jeyerson@gmail.com>
2013-04-24 21:18:57 -0500

    partially implemented client-server

--- Runner.java
+++ Runner.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
 * Bayou
 * @author Jeff
@@ -5,18 +10,74 @@
 
 public class Runner
 {
-	private static String scriptName;
+	private static int startingPort = 9000;
+	private final static String scriptName = "sample_script";
+	private static HashMap<Integer, Integer> serverPorts = new HashMap<Integer, Integer>();
+	private static HashMap<Integer, Client> clients = new HashMap<Integer, Client>();
 	
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Chao.  Welcome to Bayou.");
-		if(args.length == 1)
-			scriptName = args[0];
-		run();
 	}
 	
-	private void run(){
-		
+	private void startClient(Integer cID, Integer server)
+	{
+		Client client = new Client(serverPorts.get(server));
+		clients.put(cID, client);
 	}
 	
+	private void join(Integer sID)
+	{
+		//TODO
+	}
+	
+	private void clientDisconnect(Integer cID)
+	{
+		//TODO
+	}
+	
+	private void clientReconnect(Integer cID, Integer sID)
+	{
+		//TODO
+	}
+	
+	private void pause()
+	{
+		//TODO
+	}
+	
+	private void printLogs()
+	{
+		//TODO
+	}
+	
+	private void printLog(Integer sID)
+	{
+		//TODO
+	}
+	
+	private void isolate(Integer sID)
+	{
+		//TODO
+	}
+	
+	private void reconnect(Integer sID)
+	{
+		//TODO
+	}
+	
+	private void breakConnection(Integer sA, Integer sB)
+	{
+		//TODO
+	}
+	
+	private void recoverConnection(Integer sA, Integer sB)
+	{
+		//TODO
+	}
+	
+	private void leave(Integer sID)
+	{
+		//TODO
+	}
 }
