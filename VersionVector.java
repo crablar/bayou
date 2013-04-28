9f94ab771b21c7d09a854400eb843e22762c59a7
venkaty <venkat.yerramsetti@yahoo.com>
2013-04-28 18:46:02 -0500

    Adding anti-entropy code.

--- VersionVector.java
+++ VersionVector.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
 
 
 public class VersionVector {
@@ -9,9 +11,53 @@ public class VersionVector {
 	{
 		vector = new HashMap<Integer, Long>();
 	}
+	
+	public VersionVector(String stringifiedVector) {
+		vector = new HashMap<Integer, Long>();
+		
+		String[] pairs = stringifiedVector.split(":::");
+		Scanner parsePairs;
+		
+		int key;
+		long stamp;
+		
+		for(String s: pairs) {
+			parsePairs = new Scanner(s);
+			key = parsePairs.nextInt();
+			stamp = parsePairs.nextLong();
+			
+			vector.put(key, stamp);
+			parsePairs.close();
+		}
+	}
 
 	public void changeLatestAccept(int sID, long acceptStamp) {
 		vector.put(sID, acceptStamp);
+	}
+	
+	public void changeifMax(int sID, long acceptStamp) {
+		if(vector.get(sID) > acceptStamp) {
+			vector.put(sID, acceptStamp);
+		}
+	}
+	
+	public String toString() {
+		String res = "";
+		
+		Iterator<Integer> it = (vector.keySet()).iterator();
+		int id;
+		
+		while(it.hasNext()) {
+			id = it.next();
+			
+			res += id + " " + vector.get(id);
+
+			if(it.hasNext()) {
+				res += ":::";
+			}
+		}
+		
+		return res;
 	}
 	
 }
